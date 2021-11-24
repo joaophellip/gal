ad1f8d5c94782f30a09b4495f1ee24b970710bac
Joao Mello <joaophellip@cozo.com.br>
2021-11-24 13:31:17 -0300

    first commit implement hello world add github workflow for CI

---
+++ test/index.spec.js
@@ -0,0 +1,16 @@
+import "should"
+
+describe("API entrypoint tests", function () {
+
+    describe("invoke index.main", function () {
+      it("should return a string 'Hello World'", async function () {
+
+        const index = await import("../src/index.js")
+        const response = index.main()
+
+        response.should.be.a.String()
+        response.should.equal('Hello World')
+      })
+    })
+
+})
